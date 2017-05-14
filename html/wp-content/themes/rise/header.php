1a3fc30a2932492030a7b3a6a0970747ed79ccad
Jonathan Willingham <jwillingham1167@gmail.com>
2017-05-14 16:42:11 -0400

    Setting up theme header and testing gulp

---
+++ html/wp-content/themes/rise/header.php
@@ -0,0 +1,19 @@
+<?php
+/*
+* Rise - Header
+*/
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php bloginfo( 'name' ); ?></title>
+    <?php wp_enqueue_style( 'styles', get_stylesheet_uri() ); ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <header id="header" <?php if (!is_home()) :?>class="header-resize"<?php endif; ?>>
+        <p>Header!</p>
+    </header>
