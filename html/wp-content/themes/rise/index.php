255a506d0e09813330dcb612f590333750b85fb6
Jonathan Willingham <jwillingham1167@gmail.com>
2017-05-16 16:34:44 -0400

    Adding in the tools container

--- html/wp-content/themes/rise/index.php
+++ html/wp-content/themes/rise/index.php
@@ -9,5 +9,22 @@
 <div class="homepage-landing">
     <h1 class="landing-title">Jonathan Willingham</h1>
 </div>
+<div class="tools-container">
+    <h2>Tools I Use</h2>
+    <div class="tool-images">
+        <div class="image-container">
+            <img src="http://sass-lang.com/assets/img/styleguide/color-1c4aab2b.png" alt="Sass" />
+        </div>
+        <div class="image-container">
+            <img src="https://raw.githubusercontent.com/gulpjs/artwork/master/gulp-2x.png" alt="Gulp" />
+        </div>
+        <div class="image-container">
+            <img src="https://git-scm.com/images/logos/2color-lightbg@2x.png" alt="Git"  />
+        </div>
+        <div class="image-container">
+            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/JQuery_logo.svg/1280px-JQuery_logo.svg.png" alt="jQuert" />
+        </div>
+    </div>
+</div>
 
 <?php get_footer(); ?>
